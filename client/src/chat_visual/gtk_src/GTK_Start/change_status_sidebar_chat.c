5716896f82c389436addaf6fab85fe5a991f59db
deminkmnn <148581675+deminkmnn@users.noreply.github.com>
2024-11-27 13:27:41 +0000

    Delete client directory

--- client/src/chat_visual/gtk_src/GTK_Start/change_status_sidebar_chat.c
+++
@@ -1,6 +0,0 @@
-#include "GTK.h"
-
-void change_status_sidebar_chat(chat_data_t *chat, bool is_online) {
-    gtk_widget_remove_css_class(chat->status_indicator, is_online ? "status-offline" : "status-online");
-    gtk_widget_add_css_class(chat->status_indicator, is_online ? "status-online" : "status-offline");
-}
