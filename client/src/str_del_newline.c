d580b81496ec64057367795247923a17c9ca3524
deminkmnn <148581675+deminkmnn@users.noreply.github.com>
2024-11-29 20:54:53 +0200

    Add files via upload

---
+++ client/src/str_del_newline.c
@@ -0,0 +1,10 @@
+
+void str_del_newline(char* arr, int length) {
+    for (int i = 0; i < length; i++) {
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+
