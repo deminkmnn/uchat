b90a7d2bc919749e0d3a19f4412353f8f593c215
deminkmnn <148581675+deminkmnn@users.noreply.github.com>
2024-12-06 19:30:36 +0200

    Delete client directory

--- client/src/chat_visual/gtk_src/profile_form/reenable_button_after_delay.c
+++
@@ -1,7 +0,0 @@
-#include "GTK.h"
-
-gboolean reenable_button_after_delay(gpointer user_data) {
-    GtkButton *button = GTK_BUTTON(user_data);
-    gtk_widget_set_sensitive(GTK_WIDGET(button), TRUE); // Re-enable the button
-    return G_SOURCE_REMOVE; // Stop the timeout callback
-}
